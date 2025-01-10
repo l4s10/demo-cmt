c94db3278114f589db32fdbd2ba8997e62c69a29
Francisco Muñoz <franciscomunoz142@gmail.com>
2025-01-10 12:12:41 -0300

    Agrega contacto programador

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,8 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p>© 2023 CMT Cielos Modulares y Terminaciones</p>
+            {/* Enlace al LinkedIn del desarrollador */}
+            <a href="https://www.linkedin.com/in/franciscomv2001dev" target="_blank" rel="noreferrer">Desarrollado por Francisco Muñoz</a>
         </footer>
     );
 }
