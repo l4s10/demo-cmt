cda1dec3d46b5a40f374166842643f826a514556
Francisco Munoz <franciscomunoz142@gmail.com>
2025-01-28 12:06:43 -0300

    modifica area de contacto

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/navbar/Navbar";
 import LandingContact from "./LandingContact";
+import Footer from "../../components/footer/Footer";
 
 const Contact = () => {
     return (
         <>
             <Navbar/>
             <LandingContact/>
+            <Footer/>
         </>
     )
 }
