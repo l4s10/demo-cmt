9caf133e08d5890f53db6630990b7e88a78e87d6
Francisco Muñoz <franciscomunoz142@gmail.com>
2025-02-13 17:15:42 -0300

    modifica estructura de area contacto

--- src/pages/contact/LandingContact.tsx
+++ src/pages/contact/LandingContact.tsx
@@ -1,7 +1,9 @@
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import './LandingContact.css';
-// import Footer from '../../components/footer/Footer';
 
 const Contact = () => {
     const { ref, inView } = useInView({
@@ -20,11 +22,39 @@ const Contact = () => {
             >
                 <h2>Contacto</h2>
                 <p>Para más información, contáctanos a través de los siguientes medios:</p>
-                <ul>
-                    <li>Teléfono: +56 9 7611 5563</li>
-                    <li>Email: cmtterminaciones@gmail.com</li>
-                    <li>Dirección: Paicaví 1880, 4060598 Concepción, Bío Bío, Chile</li>
-                </ul>
+
+                <div className="contact-content">
+                    {/* Columna de información de contacto */}
+                    <div className="contact-info">
+                        <ul>
+                            <li>
+                                <FontAwesomeIcon icon={faPhone} /> Teléfono: +56 9 7611 5563
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faEnvelope} /> Email: cmtterminaciones@gmail.com
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faMapMarkerAlt} /> Dirección: Paicaví 1880, 4060598 Concepción, Bío Bío, Chile
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* Columna de redes sociales */}
+                    <div className="social-media">
+                        <h3>Síguenos en redes sociales</h3>
+                        <div className="social-icons">
+                            <a href="https://www.instagram.com/tuinstagram" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faInstagram} />
+                            </a>
+                            <a href="https://www.facebook.com/tufacebook" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faFacebook} />
+                            </a>
+                            <a href="https://www.linkedin.com/tulinkedin" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faLinkedin} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <a href="mailto:cmtterminaciones@gmail.com" target="_blank" rel="noopener noreferrer">
                     <button className="contact-button">Enviar Email</button>
                 </a>
